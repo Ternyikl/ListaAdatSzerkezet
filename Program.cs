ee1a27fe35be4cb1eafc39c989534ba0d81b5ad3
Ternyikl <ternyikl@gmail.com>
2026-02-23 12:22:18 +0100

    aha

--- Program.cs
+++ Program.cs
@@ -15,12 +15,20 @@ namespace ListaAdatSzerkezet
 			for (int i = 0; i < elemekszama; i++)
 			{
 				lista.Add(i);
+				Console.WriteLine($"Az index: {i} - capacitás: {lista.Capacity}");
 			}
 			Console.WriteLine($"Az elemek száma a listában: {lista.Count}");
 			for (int i = 0; i < lista.Count; i++)
 			{
-				Console.WriteLine(lista[i] + " ");
+				Console.Write(lista[i] + " ");
 			}
+			Console.WriteLine();
+			Console.WriteLine($"A lista első elem: {lista[0]}");
+			Console.WriteLine($"A lista utólsó elem: {lista[lista.Count - 1]}");
+			Console.WriteLine($"A lista legnagyobb elem: {lista.Max()}");
+			Console.WriteLine($"A lista legkisebb elem: {lista.Min()}");
+			Console.WriteLine();
+			
 		}
 		static void Main(string[] args)
 		{
