74a40fcbc3d8711d9b4a51c396dab667dbcd7614
Ternyikl <ternyikl@gmail.com>
2026-02-23 12:11:46 +0100

    aha2

--- Program.cs
+++ Program.cs
@@ -9,8 +9,22 @@ namespace ListaAdatSzerkezet
 	internal class Program
 	{
 		static List<int> lista = new List<int>();
+
+		static void Feltolt(int elemekszama)
+		{
+			for (int i = 0; i < elemekszama; i++)
+			{
+				lista.Add(i);
+			}
+			Console.WriteLine($"Az elemek száma a listában: {lista.Count}");
+			for (int i = 0; i < lista.Count; i++)
+			{
+				Console.WriteLine(lista[i] + " ");
+			}
+		}
 		static void Main(string[] args)
 		{
+			Feltolt(15);
 		}
 	}
 }
